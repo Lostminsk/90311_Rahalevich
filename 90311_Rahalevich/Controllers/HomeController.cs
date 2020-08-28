5cfa061498c1bce3b72bac1d5503a583f50e75ec
Aliaksandr Rahalevich <Lostminsk@gmail.com>
2020-08-28 13:00:49 +0300

    Laba_2

--- 90311_Rahalevich/Controllers/HomeController.cs
+++ 90311_Rahalevich/Controllers/HomeController.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using _90311_Rahalevich.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 
 namespace _90311_Rahalevich.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private List<ListDemo> _listDemo;
+        public class ListDemo
         {
-            _logger = logger;
+            public int ListItemValue { get; set; }
+            public string ListItemText { get; set; }
         }
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+
+      
 
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
 
